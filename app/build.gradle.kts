adf0c53cda5dfb0c28f30a5fb6014ca5aad7184f
Austin Au-Yeung <austinay96@gmail.com>
2025-12-26 19:05:22 -0500

    Update documentation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "2.8.3"
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
